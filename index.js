cf9d998a3b22f2f40af9f0572221fcd9d276ad85
Devyn Blackstone <devynblackmon@gmail.com>
2022-04-26 23:12:38 -0500

    updated index.js and seeds.sql

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.connectdb(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_db'
+    },
+    console.log("connected to employee_db")
+);
 
 
 // add prompt for questions
